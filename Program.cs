fc27a0fbb081fbb2a0956a7bfb8dbba3173761fa
anton-thomsen <anton.thomsen0@gmail.com>
2025-12-15 21:39:49 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] fraudulantOrderIDs = new string [3];
+
+fraudulantOrderIDs [0] = "A123";
+fraudulantOrderIDs [1] = "B456";
+fraudulantOrderIDs [2] = "C789";
+
+Console.WriteLine ($"First: {fraudulantOrderIDs [0]}");
+Console.WriteLine ($"Secon {fraudulantOrderIDs [1]}");
+Console.WriteLine ($"Third: {fraudulantOrderIDs [2]}");
+
+fraudulantOrderIDs [0] = "F000";
+
+Console.WriteLine ($"Reasign First: {fraudulantOrderIDs [0]}");
