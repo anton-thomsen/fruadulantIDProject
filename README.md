009eba49306c4c50d728c1c7b8dcb9c67f7d5795
anton-thomsen <anton.thomsen0@gmail.com>
2025-12-15 21:57:09 +0100

    README.md file added

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fraudulant Project
+
+## Description:
+This is a project written in C# where I wrote code to understand what arrays are and how to use them
--- obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc27a0fbb081fbb2a0956a7bfb8dbba3173761fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e509bff1cc31b00da7e8ce65ac88bb4fccf1785")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
